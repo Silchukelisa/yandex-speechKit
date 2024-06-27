092f129004b503b4db101a87094a84d2573d21b2
Silchukelisa <silchuk.liza14@gmail.com>
2024-06-27 18:09:36 +0300

    Добавление telegram API

--- src/main/java/org/tyt/bot/Bot.java
+++ src/main/java/org/tyt/bot/Bot.java
@@ -2,21 +2,32 @@ package org.tyt.bot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendVoice;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.tyt.synthesis.Synthesizer;
 
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.IOException;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
 
+    @Autowired
+    private Synthesizer synthesizer;
+
+    String text = "Привет! Это бот для синтеза речи. \n" + "Используй команду /text, чтоб озвучить желаемый текст.\n" +
+            "Пример: /text Привет, поиграем в древний ужас?";
     private static final Logger LOG = LoggerFactory.getLogger(Bot.class);
     private static final String START = "/start";
+    private static final String TEXT = "/text";
 
     public Bot(@Value("${telegram.bot-token}") String botToken) {
         super(botToken);
@@ -28,11 +39,20 @@ public class Bot extends TelegramLongPollingBot {
         if(!update.hasMessage() || !update.getMessage().hasText()){
             return;
         }
-        String message = update.getMessage().getText();
+        String[] message = update.getMessage().getText().split(" ", 2);
         Long chatId = update.getMessage().getChatId();
-        switch (message){
-            case START :
-                sendMessage(chatId,new File("result.wav"));
+        switch (message[0]){
+            case START -> {
+                 sendMessage(chatId,text);
+            }
+            case TEXT -> {
+                try {
+                    synthesizer.synthesize(message[1], new File("result.wav"));
+                } catch (UnsupportedAudioFileException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+                sendMessage(chatId, new File("result.wav"));
+            }
         }
     }
 
@@ -42,7 +62,6 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     private void sendMessage(Long chatId, File voiceFile){
-        //String chatIdStr = String.valueOf(chatId);
         SendVoice sendVoice = new SendVoice();
         sendVoice.setChatId(chatId);
         sendVoice.setVoice(new InputFile(voiceFile));
@@ -52,5 +71,13 @@ public class Bot extends TelegramLongPollingBot {
             LOG.error("Ошибка отправки сообщения", e);
         }
     }
-
+    private void sendMessage(Long chatId, String text){
+        String chatIdStr = String.valueOf(chatId);
+        var sendMessage = new SendMessage(chatIdStr,text);
+        try{
+            execute(sendMessage);
+        } catch (TelegramApiException e){
+            LOG.error("Ошибка отправки сообщения", e);
+        }
+    }
 }
